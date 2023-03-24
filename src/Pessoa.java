43b2f7f8fbf5a93da47448977b202d614c22e7f3
matheusgash <matheusxDD1@hotmail.com>
2023-03-23 21:25:49 -0300

    adicionando construtor para classe pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -2,4 +2,10 @@ public class Pessoa {
     String nome;
     String idade;
     String tamanho;
+
+    public Pessoa(String nome, String idade, String tamanho) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tamanho = tamanho;
+    }
 }
